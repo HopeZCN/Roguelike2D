bacea0b3117ae7c3459efa21adcb370477f54b21
HopeZCN <121518985@qq.com>
2016-09-12 22:38:46 +0800

    add

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Physics2D.IgnoreCollision(this.transform.gameObject.GetComponent<Collider2D>(), GameObject.Find("Player").GetComponent<Collider2D>());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
